d7ed750b46dbee81a24f2c59b26fd3689947a363
beraakash884-del <beraakash884@gmail.com>
2026-05-26 15:59:55 +0530

    My first C++ program.

---
+++ HelloWord.cpp
@@ -0,0 +1,23 @@
+// here include the header files
+
+#include <iostream>
+
+int main()
+{
+
+    // for printing strings
+
+    std::cout << "Hello World";
+
+    // Add new line use "\n" or you can use std::endl
+
+    std::cout << "\n"
+              << "Hello Word\n";
+    // or
+    //    std::cout << "\nHello Word" ;
+
+    std::cout << "hello world" << std::endl;
+    std::cout << "Akash";
+
+    return 0;
+}
