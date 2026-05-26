5fa7d92420821856721ef474dae8a8c01fd19b46
beraakash884-del <beraakash884@gmail.com>
2026-05-26 16:37:10 +0530

    Input and Output in c++

---
+++ InputOutput.cpp
@@ -0,0 +1,21 @@
+
+//  IN this time instead of writing multiple Header I will use a header file that includes all.
+// we will also use namespace so don't  need to write std:: multiple times.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hey Today we will be learing about talking input and show output \n";
+    // Todays code
+    int x, y;
+    // we can take input using cin
+    cout<<"Enter the value of x:\n";
+    cin >> x;
+    cout<<"Enter the value of y:\n";
+    cin >> y;
+    cout << "Value of x is :" << x << "\n";
+    cout << "Value of y is :" << y << "\n";
+    return 0;
+}
